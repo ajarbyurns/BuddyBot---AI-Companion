4c5d52b92d3aba41bd854eecfdb51ee20d0102f8
Barry Juans <barry.juans.a@gmail.com>
2025-08-06 13:49:03 +0700

    Initial Commit

---
+++ AICompanion/AICompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  AICompanionApp.swift
+//  AICompanion
+//
+//  Created by Barry Juans on 06/08/25.
+//
+
+import SwiftUI
+
+@main
+struct AICompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AICompanion/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AICompanion
+//
+//  Created by Barry Juans on 06/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
